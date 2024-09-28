8208e382d02ce9de10f9c56bb2fc7d8821d38a42
h1lsx <hererr2@gmail.com>
2024-09-28 15:22:21 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,7 @@ const cacheName = "cache-v3";
 const precachedResources = ['index.html', 'styles.css', 'app.js', 'pwa.png', 'files/index.html', 'files/Build/slope.json', 'files/Build/slope_data.unityweb', 'files/Build/slope_framework.unityweb', 'files/Build/slope_memory.unityweb', 'files/Build/slope_wasmcode.unityweb', 'files/Build/slope_wasmframework.unityweb', 'files/TemplateData/UnityProgress.js', 'files/TemplateData/ProgressEmpty.Dark.png', 'files/TemplateData/ProgressFull.Dark.png', 'files/TemplateData/ProgressLogo.Dark.png', 'files/TemplateData/style.css', 'files/TemplateData/unityloader41.js'];
 
 async function precache() {
+  console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
   caches.open(cacheName).then((cache) => {
       console.log(cache, precachedResources);
       return cache.addAll(precachedResources);
