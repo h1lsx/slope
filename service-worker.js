3f60ee174875d10dc8d9b52dee3e7bd1a956e064
h1lsx <hererr2@gmail.com>
2024-09-28 15:06:45 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,11 @@ const cacheName = "cache-v3";
 const precachedResources = ['index.html', 'styles.css', 'app.js', 'pwa.png', 'files/index.html', 'files/Build/slope.json', 'files/Build/slope_data.unityweb', 'files/Build/slope_framework.unityweb', 'files/Build/slope_memory.unityweb', 'files/Build/slope_wasmcode.unityweb', 'files/Build/slope_wasmframework.unityweb', 'files/TemplateData/UnityProgress.js', 'files/TemplateData/ProgressEmpty.Dark.png', 'files/TemplateData/ProgressFull.Dark.png', 'files/TemplateData/ProgressLogo.Dark.png', 'files/TemplateData/style.css', 'files/TemplateData/unityloader41.js'];
 
 async function precache() {
-  const cache = await caches.open(cacheName);
-  return cache.addAll(precachedResources);
+  caches.open(cacheName).then((cache) => {
+      console.log(cache, precachedResources);
+      return cache.addAll(precachedResources);
+    }
+  )
 }
 
 self.addEventListener("install", (event) => {
